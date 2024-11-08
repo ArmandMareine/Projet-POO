6b04e222fa421e5feb507079eb821a9b620f3bfc
Aubin <linjiaxinaubin@gmail.com>
2024-11-08 13:08:10 +0100

    Classe Dé et Plateau commenté

--- Projet_Algo_1/Plateau.cs
+++ Projet_Algo_1/Plateau.cs
@@ -13,14 +13,14 @@ namespace Projet_Algo_1
 
         public Plateau(int taille)
         {
-            this.taille = taille;
-            plateau = new Dé[taille,taille];
+            this.taille = taille; /// Initialisation de la taille entrée par l'utilisateur en tant que taille de la classe plateau
+            plateau = new Dé[taille, taille];/// Initialise le plateau en tant que matrice carré de Dé de taille : taille * taille
 
             for (int i = 0; i < taille; i++)
             {
-                for(int j = 0; j < taille; j++)
+                for (int j = 0; j < taille; j++)
                 {
-                    plateau[i, j] = new Dé();
+                    plateau[i, j] = new Dé(); /// Initialisation de chaque case de la matrice en tant qu'objet de la classe Dé
                 }
             }
 
@@ -28,15 +28,14 @@ namespace Projet_Algo_1
 
         public void LancerTousLesDés()
         {
-            for(int i = 0;i < taille; i++)
+            for (int i = 0; i < taille; i++)
             {
-                for(int j = 0;j < taille; j++)
+                for (int j = 0; j < taille; j++)
                 {
-                    char résultat = plateau[i,j].Lancé();
-                    Console.Write(résultat + "\t");
+                    char résultat = plateau[i, j].Lancé(); /// Parcour chaque case du plateau et lance le dé puis initialise la variable résultat avec la lettre obtenue avec le lancé de Dé
+                    Console.Write(résultat + "\t"); /// Affichage du plateau
                 }
-                Console.WriteLine("\n\n\n");
-
+                Console.WriteLine("\n\n\n");/// Espacement pour la clarité du plateau
             }
         }
     }
