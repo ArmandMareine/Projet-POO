729862d60ab929e0d006c39c91f8efb3cefb2bf3
Armand <armand.mareine@edu.devinci.fr>
2024-11-08 21:07:08 +0100

    Update

--- Projet_Algo_1/Program.cs
+++ Projet_Algo_1/Program.cs
@@ -6,10 +6,28 @@
         {
             Console.Write("Entrez la taille du plateau (par exemple, 3 pour un plateau 3x3) : ");
             int taille = int.Parse(Console.ReadLine());
+            Console.Write("Saisir le temps désiré pour la partie en minutes (par exemple, 2 pour 2 minutes de partie): ");
+            int tempstotal=int.Parse(Console.ReadLine())*60;
+            Console.Write("Saisir le temps de jeu par joueur en minutes (par exemple, 2 pour 2 minutes de jeu):");
+            int tempsjoueur=int.Parse(Console.ReadLine())*60;   
 
+            Temps temps = new Temps(tempstotal, tempsjoueur);///Appel au constructeur naturel pour initialisation
             Plateau plateau = new Plateau(taille);
-            Console.WriteLine("Voici votre plateau :");
-            plateau.LancerTousLesDés();
+            ///Lancement de la partie 
+            Console.WriteLine("La partie commence ! ");
+            
+            while (tempstotal> 0)///Tant que le temps n'est pas écoulé, la partie continue
+            {
+                tempstotal--;
+                Console.WriteLine("Voici votre plateau :");
+                plateau.LancerTousLesDés();
+                Thread.Sleep(tempsjoueur);///On laisse le temps au joueur de jouer avant d'afficher un nouveau plateau
+
+            }
+           
+            
+           
+
         }
     }
 }
