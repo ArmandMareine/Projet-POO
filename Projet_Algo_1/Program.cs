21fb228e2c51f9f6fb31165b6d55385ab3f206cc
Armand <armand.mareine@edu.devinci.fr>
2024-11-09 13:05:52 +0100

    Classe joueur et programme principal fin pour ajd

--- Projet_Algo_1/Program.cs
+++ Projet_Algo_1/Program.cs
@@ -7,11 +7,11 @@ namespace Projet_Algo_1
         static void Main(string[] args)
         {
             Console.WriteLine("Bienvenue au jeu du Boggle ! ");
-            Console.WriteLine("Saisir la langue désirée : (Anglais ou Français)");///On définit la langue utilisée
+            Console.Write("Saisir la langue désirée (Anglais ou Français) : ");///On définit la langue utilisée
             string langue = Convert.ToString(Console.ReadLine()); 
             if(langue != "Anglais" || langue!= "Français")
             {
-                Console.Write("Le format est incorrect ! Format attendu : Anglais ou Français");///En cas d'erreur de saisie, on recommence
+                Console.Write("Le format est incorrect ! Format attendu : Anglais ou Français  ");///En cas d'erreur de saisie, on recommence
                 langue = Convert.ToString(Console.ReadLine());
             }
             Console.Write("Saisir le nombre de joueurs voulus (Attention, le minimum est 2 et le maximum 10) : ");///On saisit le nombre de joueurs
@@ -32,7 +32,7 @@ namespace Projet_Algo_1
             int taille = int.Parse(Console.ReadLine());
             Console.Write("Saisir le temps désiré pour la partie en minutes (par exemple, 2 pour 2 minutes de partie): ");
             int tempstotal=int.Parse(Console.ReadLine())*60;
-            Console.Write("Saisir le temps de jeu par joueur en minutes (par exemple, 1 pour 1 minute de jeu");
+            Console.Write("Saisir le temps de jeu par joueur en minutes (par exemple, 1 pour 1 minute de jeu) :");
             int tempsjoueur=int.Parse(Console.ReadLine())*60;   
 
             Temps temps = new Temps(tempstotal, tempsjoueur);///Appel au constructeur naturel pour initialisation
@@ -42,25 +42,26 @@ namespace Projet_Algo_1
             
             while (tempstotal> 0)///Tant que le temps n'est pas écoulé, la partie continue
             {
-                tempstotal--;
+                
                 for(int i =0; i<joueurs.Length; i++)
                 {
                     Console.WriteLine($"C'est au tour du joueur {i+1} : {joueurs[i]} de jouer !");///On donne le temps de jeu au joueur
-                    Console.WriteLine($"Ton temps de jeu est de {tempsjoueur} minutes");///On donne le temps de jeu au joueur 
+                    Console.WriteLine($"Ton temps de jeu est de {tempsjoueur/60} minutes");///On donne le temps de jeu au joueur 
                     Console.WriteLine("Voici ton plateau :");
                     plateau.LancerTousLesDés();
                     while (tempsjoueur > 0)
                     {
                         ///Le jeu se déroule ici 
 
-
+                        Console.WriteLine("Hello ! ");
                         tempsjoueur--;
-                        Thread.Sleep(1000);///Temps d'une seconde entre chaque itération
+                        
                     }
+                    Thread.Sleep(1000);///Temps d'une seconde entre chaque itération
 
-                   
 
                 }
+                tempstotal--;
                 Thread.Sleep(tempsjoueur);///On laisse le temps au joueur de jouer avant d'afficher un nouveau plateau
 
 
