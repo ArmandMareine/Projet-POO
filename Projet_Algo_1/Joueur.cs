24fdf27d260b7d4ae7d34dd5f748603f77289246
Aubin <linjiaxinaubin@gmail.com>
2024-11-09 12:35:19 +0100

    Update : Ajout de la classe joueur avec l'attribut pseudo et score set à 0

---
+++ Projet_Algo_1/Joueur.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projet_Algo_1
+{
+    internal class Joueur
+    {
+        private string pseudo;
+        private int score;
+
+        public Joueur(string pseudo, int score)
+        {
+            this.pseudo = pseudo;
+            this.score = score;
+            score = 0;
+        }
+    }
+}
