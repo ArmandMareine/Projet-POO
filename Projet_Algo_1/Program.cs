68ca0d61c76814d7aaff7a332acebc8c0df842c9
Armand <armand.mareine@edu.devinci.fr>
2024-12-04 18:41:49 +0100

    Modification dans le programme principal avec une refonte de l'ordre du jeu et un tri dans les choses inutiles. Création du timer

--- Projet_Algo_1/Program.cs
+++ Projet_Algo_1/Program.cs
@@ -3,7 +3,7 @@ using System.Globalization;
 
 namespace Projet_Algo_1
 {
-    internal class Program
+    internal class Jeu
     {
         static void Main(string[] args)
         {
@@ -34,29 +34,33 @@ namespace Projet_Algo_1
             {
                 Console.WriteLine("Aucune lettre n'a été importée.");
             }
-
-
-            
-
-
-            ///Jeu codé
             /*
-            foreach (string str in tri)
-            {
-                Console.WriteLine(string.Join(",", tri));
-            }
-            Console.WriteLine("Ok");
-            
+           foreach (string str in tri)
+           {
+               Console.WriteLine(string.Join(",", tri));
+           }
+           Console.WriteLine("Ok");
 
-            
-            foreach (string mot in list)
-            {
-                Console.WriteLine(mot);
-            }
-            */
-           ///On initialise le chemin pour le fichier Lettres
+
+
+           foreach (string mot in list)
+           {
+               Console.WriteLine(mot);
+           }
+           */
+
+
+
+
+
+
+
+            ////////////////////////////////////////////////////////////////////////
+            //Fin de la zone de test : Début du jeu 
             Console.WriteLine("Bienvenue au jeu du Boggle ! ");
-            string cheminFichier3 = "";
+            ///Définition des chemins fichiers à rajouter (le lettre)
+            ///Défintion des timers
+            
             Console.Write("Saisir la langue désirée (Anglais ou Français):");///On définit la langue utilisée
             string langue = Convert.ToString(Console.ReadLine());
             string EN = "Anglais";
@@ -79,14 +83,10 @@ namespace Projet_Algo_1
                     Console.Write("Le format est incorrect ! Format attendu : Anglais ou Français  ");///En cas d'erreur de saisie, on recommence
                     langue = Convert.ToString(Console.ReadLine());
                 } while (!fr && !en);
-            }
-            ///Test des conditions sur le fichier 
-            
-            
-            
+            }///Test de la bonne saisie des langues 
+
             if (langue == "Anglais" || langue == "Français")
             {
-                
                 Console.Write("Saisir le nombre de joueurs voulus (Attention, le minimum est 2 et le maximum 10) : ");///On saisit le nombre de joueurs
                 int nbjoueurs = int.Parse(Console.ReadLine());
                 Joueur[] joueurs = new Joueur[nbjoueurs];///Définition d'un tableau de joueur pour contenir les informations chaque participant
@@ -102,77 +102,66 @@ namespace Projet_Algo_1
                 {
                     Console.WriteLine(joueur.ToString());
                 }
-                Console.Write("Entrez la taille du plateau (par exemple, 4 pour un plateau 4x4) : ");
+                Console.Write("Entrez la taille du plateau (par exemple, 4 pour un plateau 4x4) : ");///Initialisation de la taille du plateau
                 int taille = int.Parse(Console.ReadLine());
                 Console.Write("Saisir le temps désiré pour la partie en minutes (par exemple, 2 pour 2 minutes de partie): ");
                 int tempstotal = int.Parse(Console.ReadLine()) * 60;
+                TimeSpan dureetotale = TimeSpan.FromMinutes(tempstotal);///Initialisation du premier timer, le timer pour le jeu total
                 Console.Write("Saisir le temps de jeu par joueur en minutes (par exemple, 1 pour 1 minute de jeu) :");
                 int tempsjoueur = int.Parse(Console.ReadLine()) * 60;
-
-                Temps temps = new Temps(tempstotal, tempsjoueur);///Appel au constructeur naturel pour initialisation
-                //Plateau plateau = new Plateau(taille);
-                ///Lancement de la partie 
+                TimeSpan dureeparjoueur = TimeSpan.FromMinutes(tempsjoueur); ///Initialisation du temps de jeu par joueur à une valeur par défaut de 1 minute
+                Console.WriteLine($"Le temps total de la partie est donc de : {dureetotale} minutes");
+                Console.WriteLine($"Le temps de jeu pour chaque joueur est de : {dureeparjoueur}");
+                ///Début du jeu 
                 Console.WriteLine("La partie commence ! ");
-                
-                while (tempstotal > 0)///Tant que le temps n'est pas écoulé, la partie continue
-                {
+                DateTime debutPartie = DateTime.Now;///Initialisation du début de la partie au moment de son lancement
 
+                while (DateTime.Now - debutPartie < dureetotale)
+                {
                     for (int i = 0; i < joueurs.Length; i++)
                     {
                         Console.WriteLine($"C'est au tour du joueur {i + 1} : {joueurs[i]} de jouer !");///On donne le temps de jeu au joueur
                         Console.WriteLine($"Ton temps de jeu est de {tempsjoueur / 60} minutes");///On donne le temps de jeu au joueur 
                         Console.WriteLine("Voici ton plateau :");
-                        //List<Lettre> lettres = Lettre.LectureFichier(cheminFichier2);
-                        Console.WriteLine("Lettres chargées depuis le fichier :");
-                        foreach (var lettre in lettres)
-                        {
-                            Console.WriteLine($"Caractère : {lettre.Caractere}, Valeur : {lettre.Valeur}, Nombre : {lettre.Nombre}");
-                        }
-                        //plateau.LancerTousLesDés(lettres, taille);
-                        //Console.WriteLine(plateau.ToString());///Le plateau du joueur s'affiche à l'écran - PROBLEME 
-                        while (tempsjoueur > 0)
-                        {
-                            
-                            ///Le jeu se déroule ici 
+                        ///Affichage du plateau
+                        ///SUITE DU JEU
 
-                            Console.WriteLine("Hello ! ");
-                            tempsjoueur--;
-                            Thread.Sleep(1000);///Temps d'une seconde entre chaque itération
-                                               ///Utilisation de la méthode 1 pout le tri et la recherche du mot
-                            
-                            
-                            //List<string> motstriés = Tri_Fichier_2.TriparFusion(mots);
-                            string motchercher = "Arbre";
-                            try
-                            {
-                                /*
-                                if (Tri_Fichier_2.RechercheDichotomique(mots, motchercher)==true)
-                                {
-                                    Console.WriteLine($"Le mot {motchercher} est bien dans le dictionnaire !");
-                                }
-                                else
-                                {
-                                    Console.WriteLine("Il n'y est pas");
-                                }
-                                */
-                            }
-                            catch(FileNotFoundException ex)
-                            {
-                                Console.WriteLine(ex.Message);
-                            }
-                            
-                        }
-                    
+
+
+
 
 
                     }
-                    //tempstotal--;
-                    //Thread.Sleep(tempsjoueur);///On laisse le temps au joueur de jouer avant d'afficher un nouveau plateau
-                    
+
+
+                    DateTime debutTour = DateTime.Now;
+                    while (true)///Test en permanence
+                    {
+
+                        if (DateTime.Now - debutTour > dureeparjoueur)///On teste si le temps de jeu du joueur est écoulé
+                        {
+                            Console.WriteLine("Le temps pour le joueur X est écoulé");
+                            break;
+                        }
+                        if (Console.KeyAvailable)///Si le joueur appuie sur une touche, la boucle s'arrête 
+                        {
+                            Console.ReadKey();
+                            Console.WriteLine("Action du joueur enregistrée");
+                            break;
+                        }
+                    }
+                    if (DateTime.Now - debutPartie > dureetotale)///Fin du jeu : test
+                    {
+                        Console.WriteLine("Temps total du jeu du Boggle écoulé ! ");
+                        break;
+                    }
+
+
                 }
-            
-            }
-                
+                Console.WriteLine("Fin de la partie ! Merci ! ");///Affichage fin de partie 
+
+
+            }   
            
         }
             
